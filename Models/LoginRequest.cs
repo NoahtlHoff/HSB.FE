d6db5ee594a502c95bb5a52d3a79642d12dcaa51
Noah <noah.hoff@outlook.com>
2025-11-12 16:30:02 +0100

    reg/login models

---
+++ Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace HackStreeBoys_Website.Models
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
