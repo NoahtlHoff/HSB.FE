764d4bb77d6b0f4aaee4b499df161b98e75b3514
Noah <noah.hoff@outlook.com>
2025-11-12 16:30:35 +0100

    service for auth/api

---
+++ Service/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using HackStreeBoys_Website.Models;
+
+namespace HackStreeBoys_Website.Service
+{
+    public class AuthService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<LoginResponse> LoginAsync(LoginRequest loginRequest)
+        {
+            var client = _httpClientFactory.CreateClient("API");
+
+            var response = await client.PostAsJsonAsync("/api/auth/login", loginRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<LoginResponse>();
+            }
+
+            return null;
+        }
+
+        public async Task<RegisterResponse> RegisterAsync(RegisterRequest registerRequest)
+        {
+            var client = _httpClientFactory.CreateClient("API");
+
+            var response = await client.PostAsJsonAsync("/api/auth/register", registerRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<RegisterResponse>();
+            }
+
+            return null;
+        }
+    }
+}
